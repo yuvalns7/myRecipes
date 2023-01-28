dfcbfeefbabd2cadcb6fab6b1bf8820e4e86f882
yuvalns7 <yuvalns7@gmail.com>
2023-01-28 17:29:47 +0200

    login component

---
+++ app/src/main/java/com/example/myrecipes/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myrecipes.model.user.User;
+import com.example.myrecipes.model.user.UserModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView createNewAccount;
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createNewAccount = findViewById(R.id.createNewAccount);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        progressDialog = new ProgressDialog(this);
+
+        createNewAccount.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
+    }
+}
