313bcb12cce025e5f1b5a29bf75149c5b600b57d
yuvalns7 <yuvalns7@gmail.com>
2023-01-29 18:23:59 +0200

    remove user dao

--- app/src/main/java/com/example/myrecipes/model/localDB/AppLocalDbRepository.java
+++ app/src/main/java/com/example/myrecipes/model/localDB/AppLocalDbRepository.java
@@ -6,11 +6,8 @@ import androidx.room.RoomDatabase;
 import com.example.myrecipes.model.recipe.Recipe;
 import com.example.myrecipes.model.recipe.RecipeDao;
 import com.example.myrecipes.model.user.User;
-import com.example.myrecipes.model.user.UserDao;
 
 @Database(entities = {Recipe.class, User.class}, version = 1)
 public abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract RecipeDao recipeDao();
-
-    public abstract UserDao userDao();
 }
